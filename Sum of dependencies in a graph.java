612ced773cb03481827868a08ede9b0b916981a8
Mate Chaitanya Rajendra <99747639+MateChaitanya@users.noreply.github.com>
2023-11-28 20:59:40 +0530

    Create Sum of dependencies in a graph.java

---
+++ Sum of dependencies in a graph.java
@@ -0,0 +1,10 @@
+class Solution {
+    int sumOfDependencies(ArrayList<ArrayList<Integer>> adj, int V) {
+        // code here
+        int res = 0;
+        for(ArrayList<Integer> list : adj){
+            res+=list.size();
+        }
+        return res;
+    }
+};
