9db20af0c10492aa66b3c5db78c363130b10fb59
Mate Chaitanya Rajendra <99747639+MateChaitanya@users.noreply.github.com>
2024-04-10 08:11:00 +0530

    Create Party of Couples.java

---
+++ Party of Couples.java
@@ -0,0 +1,10 @@
+class Solution{
+    static int findSingle(int n, int arr[]){
+        // code here
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result ^= arr[i];
+        }
+        return result;
+    }
+}
