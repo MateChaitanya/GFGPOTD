2a6b4e34c56389d894759cbe7f2cb871a5e5fcfc
Mate Chaitanya Rajendra <99747639+MateChaitanya@users.noreply.github.com>
2024-08-03 19:25:26 +0530

    Create The Celebrity Problem.java

---
+++ The Celebrity Problem.java
@@ -0,0 +1,17 @@
+ public int celebrity(int mat[][]) {
+        // code here
+        int ans=-1,j=0,n=mat.length;
+        for(int i=0;i<n;i++){
+            for(j=0;j<n;j++){
+                if(mat[i][j]==1)break;
+            }
+            if(j==n){
+                for(j=0;j<n;j++){
+                    if(i!=j && mat[j][i]==0)break;
+                }
+                if(j==n)ans=i;
+                break;
+            }
+        }
+        return ans;
+    }
