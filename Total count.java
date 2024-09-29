a28038e40277f08ecab399f69260eb2d21cc747c
Mate Chaitanya Rajendra <99747639+MateChaitanya@users.noreply.github.com>
2024-09-29 22:36:13 +0530

    Create Total count.java

---
+++ Total count.java
@@ -0,0 +1,10 @@
+class Solution {
+    int totalCount(int k, int[] arr) {
+        // code here
+        int ans = 0;
+        for(int x : arr) {
+            ans += (x + k - 1) / k;
+        }
+        return ans;
+    }
+}
